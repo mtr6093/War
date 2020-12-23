7e6b7fe3d88139fb23e0b27e8707e76b649ab7bf
Rivers Marc <mr41498@wdxmr41498vcg.home>
2020-12-22 21:22:24 -0600

    Start on player

--- src/js/script.js
+++ src/js/script.js
@@ -23,24 +23,24 @@ function create (){ //Function that creates complete deck (Card)
   create()
 //console.log(cards)
 
-let shuffled = [...cards]
+let shuffled = [...cards] //Array of shuffled cards
 
-function shuffler(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+function shuffler(array) { //Function that shuffles cards and puts them in the shuffled array
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
     }
 }
 shuffler(shuffled)
-console.log("Shuffled", shuffled)
+//console.log("Shuffled", shuffled)
 
-let player1Hand = []
-let player2Hand = []
+let player1Hand = [] //array of player 1 cards
+let player2Hand = [] //array of player 2 cards
 
 
-function deal (){
+function deal (){ // function that deals the deck to player1 and player2
   for (let i=0; i<shuffled.length; i++){
     if ((i+2)%2==0) {
       player1Hand.push(shuffled[i])
@@ -51,4 +51,28 @@ function deal (){
   }
 }
 deal(shuffled)
-console.log("Player", player1Hand)
+//console.log("Player1", player1Hand)
+//console.log("Player2", player2Hand)
+console.log(player1Hand[0])
+let playedP1 = []
+let playedP2 = []
+
+function play () {
+  if (player1Hand.length>0) {
+    playedP1.push((player1Hand)[0])
+    player1Hand.splice(0,1)
+  } else if (player1Hand.length==0){
+    console.log("Player1 out of cards")
+  } 
+  if (player2Hand.length>0) {
+    playedP2.push((player2Hand)[0])
+  } else if (player2Hand.length==0){
+    console.log("Player1 out of cards")
+  } 
+}
+play()
+
+console.log("roundPlayed", playedP1)
+console.log(playedP1[0])
+console.log(playedP2[0])
+console.log(player1Hand[0])
